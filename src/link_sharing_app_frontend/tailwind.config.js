c5ed5015f92b6e9bb9e130c5e51d3b2205a7c9a9
Abrham007 <abrhamaraya111@gmail.com>
2024-04-05 17:25:33 +0300

    configed the font and color

--- src/link_sharing_app_frontend/tailwind.config.js
+++ src/link_sharing_app_frontend/tailwind.config.js
@@ -1,12 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      Purple: "#633CFF",
+      LightPurple: "#BEADFF",
+      VeryLightPurple: "#EFEBFF",
+      DarkGrey: "#333333",
+      Grey: "#737373",
+      LightGrey: "#FAFAFA",
+      Borders: "#D9D9D9",
+      White: "#FFF",
+      Red: "#FF3939",
+    },
+    fontSize: {
+      xl: [
+        "2rem",
+        {
+          lineHeight: "1.5",
+          letterSpacing: "normal",
+          fontWeight: "700",
+        },
+      ],
+      lg: [
+        "1rem",
+        {
+          lineHeight: "1.5",
+          letterSpacing: "normal",
+          fontWeight: "600",
+        },
+      ],
+      base: [
+        "1rem",
+        {
+          lineHeight: "1.5",
+          letterSpacing: "normal",
+          fontWeight: "400",
+        },
+      ],
+      sm: [
+        "0.75rem",
+        {
+          lineHeight: "1.5",
+          letterSpacing: "normal",
+          fontWeight: "400",
+        },
+      ],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
