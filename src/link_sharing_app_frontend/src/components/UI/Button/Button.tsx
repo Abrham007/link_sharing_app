d00f06fd68a7f29e08da79e27dde363d95e268fe
Abrham007 <abrhamaraya111@gmail.com>
2024-04-05 19:49:55 +0300

    made the component more reusable

--- src/link_sharing_app_frontend/src/components/UI/Button/Button.tsx
+++ src/link_sharing_app_frontend/src/components/UI/Button/Button.tsx
@@ -4,24 +4,31 @@ import { ButtonHTMLAttributes } from "react";
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   kind: string;
+  className?: string;
 }
-export default function Button({ children, kind }: ButtonProps) {
+export default function Button({ children, kind, className }: ButtonProps) {
   switch (kind) {
     case "1":
       return (
-        <button className="px-[27px] py-[11px] text-lg text-White bg-Purple rounded-lg hover:bg-LightPurple">
+        <button
+          className={`w-full px-[27px] py-[11px] text-lg text-White bg-Purple rounded-lg hover:bg-LightPurple ${className}`}
+        >
           {children}
         </button>
       );
     case "2":
       return (
-        <button className="px-[27px] py-[11px] text-lg text-Purple bg-transparent hover:bg-LightPurple rounded-lg border border-solid border-Purple">
+        <button
+          className={`px-[27px] py-[11px] text-lg text-Purple bg-transparent hover:bg-LightPurple rounded-lg border border-solid border-Purple ${className}`}
+        >
           {children}
         </button>
       );
     default:
       return (
-        <button className="px-[27px] py-[11px] text-lg text-White bg-Purple rounded-lg hover:bg-LightPurple">
+        <button
+          className={`w-full px-[27px] py-[11px] text-lg text-White bg-Purple rounded-lg hover:bg-LightPurple ${className}`}
+        >
           {children}
         </button>
       );
