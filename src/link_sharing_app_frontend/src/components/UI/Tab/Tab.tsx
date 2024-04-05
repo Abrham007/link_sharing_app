6a6162e16ec897fb9e61eecc2262b7ce781e8cab
Abrham007 <abrhamaraya111@gmail.com>
2024-04-05 19:50:28 +0300

    made the components more reusable

--- src/link_sharing_app_frontend/src/components/UI/Tab/Tab.tsx
+++ src/link_sharing_app_frontend/src/components/UI/Tab/Tab.tsx
@@ -1,14 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Tab({ children }: { children: React.ReactNode }) {
   return (
-    <Link
+    <a
       href="/"
-      className="flex gap-2 px-[27px] py-[11px] text-lg text-Purple bg-transparent rounded-lg active:bg-LightPurple"
+      className="flex items-center gap-2 px-[27px] py-[11px] text-lg text-Grey active:text-Purple hover:text-Purple bg-transparent rounded-lg active:bg-VeryLightPurple"
     >
-      <Image src="/images/icon-links.svg" alt="" width={16} height={16}></Image>
-      <span>{children}</span>
-    </Link>
+      {children}
+    </a>
   );
 }
