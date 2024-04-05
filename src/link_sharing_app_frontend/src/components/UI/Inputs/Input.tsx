d1f6759e8a4c0e00dd26dd8226f180cebce7d2eb
Abrham007 <abrhamaraya111@gmail.com>
2024-04-05 17:25:16 +0300

    improved the UI component

--- src/link_sharing_app_frontend/src/components/UI/Inputs/Input.tsx
+++ src/link_sharing_app_frontend/src/components/UI/Inputs/Input.tsx
@@ -1,20 +1,31 @@
-import Image from "next/image";
 import { InputHTMLAttributes } from "react";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: string;
+  icon: string;
+}
 
-export default function Input({ ...props }: InputProps) {
+export default function Input({ label, name, icon, ...props }: InputProps) {
   return (
-    <label className="flex gap-3 px-4 py-3 bg-White border border-solid border-Borders hover:border-Purple cursor-pointer">
-      <Image src="/images/icon-links.svg" alt="" width={16} height={16}></Image>
-      <input
-        {...props}
-        type="text"
-        className="w-full text-base text-DarkGrey"
-      />
-      <span className="ml-auto text-sm text-Red shrink-0">
-        Please check again
-      </span>
-    </label>
+    <div className="flex flex-col gap-1 cursor-pointer">
+      <label htmlFor={name} className="text-sm text-DarkGrey">
+        {label}
+      </label>
+      <div className="flex gap-3 items-center px-4 py-3 bg-White border-[1px] border-solid border-Borders hover:border-Purple rounded-lg">
+        <img src={icon} alt="" width={16} height={16}></img>
+        <input
+          {...props}
+          id={name}
+          type="text"
+          className="w-full text-base text-DarkGrey bg-transparent"
+        />
+        {false && (
+          <span className="ml-auto text-sm text-Red shrink-0">
+            Please check again
+          </span>
+        )}
+      </div>
+    </div>
   );
 }
