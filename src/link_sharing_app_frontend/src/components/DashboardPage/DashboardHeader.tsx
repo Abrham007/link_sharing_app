a168558d215db8310f1d98ff9f7fc890b7471e4d
Abrham007 <abrhamaraya111@gmail.com>
2024-04-08 19:54:54 +0300

    made the component interactive and imporved style

--- src/link_sharing_app_frontend/src/components/DashboardPage/DashboardHeader.tsx
+++ src/link_sharing_app_frontend/src/components/DashboardPage/DashboardHeader.tsx
@@ -4,35 +4,62 @@ import previewIcon from "/images/icon-preview-header.svg";
 import Button from "../UI/Button/Button";
 import LinkIcon from "../UI/Icons/LinkIcon";
 import DetailIcon from "../UI/Icons/DetailIcon";
+import { Link, useLocation } from "react-router-dom";
 
 export default function DashboardHeader() {
-  return (
-    <header className="w-full lg:max-w-[1392px] flex justify-between p-4 pl-6 lg:mx-auto bg-White md:rounded-xl">
-      <div className="flex gap-2 items-center">
-        <img src={logoIcon} alt="devlinks logo" width={40} height={40} />
-        <h1 className="hidden md:block text-xl text-DarkGrey">devlinks</h1>
-      </div>
-      <nav className="flex ">
-        <Tab>
-          <LinkIcon></LinkIcon>
-          <span className="hidden md:inline-block">Links</span>
-        </Tab>
-        <Tab>
-          <DetailIcon></DetailIcon>
-          <span className="hidden md:inline-block">Profile Details</span>
-        </Tab>
-      </nav>
+  const location = useLocation();
+  let activeTab = "links";
 
-      <Button kind="2">
-        <img
-          src={previewIcon}
-          alt=""
-          width={20}
-          height={20}
-          className="md:hidden"
-        />
-        <span className="hidden md:inline-block">Preview</span>
-      </Button>
+  if (location.pathname === "/dashboard/profile") {
+    activeTab = "profile";
+  }
+  return (
+    <header className="w-full lg:max-w-[1392px] flex p-4 pl-6 lg:mx-auto bg-White md:rounded-xl">
+      <nav className="w-full flex justify-between">
+        <div className="flex gap-2 items-center">
+          <img src={logoIcon} alt="devlinks logo" width={40} height={40} />
+          <h1 className="hidden md:block text-xl text-DarkGrey">devlinks</h1>
+        </div>
+        <div className="flex">
+          <Link
+            to="links"
+            className={`flex items-center gap-2 px-[27px] py-[11px] text-lg hover:text-Purple rounded-lg ${
+              activeTab === "links"
+                ? "text-Purple bg-VeryLightPurple"
+                : "text-Grey bg-transparent"
+            } `}
+          >
+            <LinkIcon></LinkIcon>
+            <span className="hidden md:inline-block">Links</span>
+          </Link>
+
+          <Link
+            to="profile"
+            className={`flex items-center gap-2 px-[27px] py-[11px] text-lg hover:text-Purple rounded-lg ${
+              activeTab === "profile"
+                ? "text-Purple bg-VeryLightPurple"
+                : "text-Grey bg-transparent"
+            } `}
+          >
+            <DetailIcon></DetailIcon>
+            <span className="hidden md:inline-block">Profile Details</span>
+          </Link>
+        </div>
+
+        <Link
+          to="preview"
+          className={`px-[27px] py-[11px] text-lg text-Purple bg-transparent hover:bg-LightPurple rounded-lg border border-solid border-Purple`}
+        >
+          <img
+            src={previewIcon}
+            alt=""
+            width={20}
+            height={20}
+            className="md:hidden"
+          />
+          <span className="hidden md:inline-block">Preview</span>
+        </Link>
+      </nav>
     </header>
   );
 }
