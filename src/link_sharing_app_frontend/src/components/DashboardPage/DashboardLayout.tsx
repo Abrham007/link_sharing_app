1c54c9ad77d0c2b9d479f0d68319bfb190404bd3
Abrham007 <abrhamaraya111@gmail.com>
2024-04-08 14:57:06 +0300

    improved the structure for the auth

---
+++ src/link_sharing_app_frontend/src/components/DashboardPage/DashboardLayout.tsx
@@ -0,0 +1,16 @@
+import CustomLinkPage from "./CustomLinkPage/CustomLinkPage";
+import DashboardHeader from "./DashboardHeader";
+
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col gap-4 md:gap-6 min-h-screen p-4 md:p-6 bg-LightGrey">
+      <DashboardHeader></DashboardHeader>
+      <main className="w-full lg:max-w-[1392px] mx-auto flex gap-6 justify-center">
+        <section className="hidden flex-1 lg:flex justify-center items-center max-w-[560px] h-[834px] px-[25px] bg-White rounded-xl ">
+          <img src="/images/illustration-phone-mockup.svg" alt="" />
+        </section>
+        <CustomLinkPage></CustomLinkPage>
+      </main>
+    </div>
+  );
+}
