01c628d1fe9a333a55566bfce692ce03ac9cb64a
Abrham007 <abrhamaraya111@gmail.com>
2024-04-09 17:28:04 +0300

    finished the login flow frontend

--- src/link_sharing_app_frontend/src/components/CreateAccountPage.tsx
+++ src/link_sharing_app_frontend/src/components/CreateAccountPage.tsx
@@ -2,8 +2,44 @@ import logoIcon from "/images/logo-devlinks-small.svg";
 import Button from "./UI/Button/Button";
 import Input from "./UI/Inputs/Input";
 import { Link } from "react-router-dom";
+import { AuthClient } from "@dfinity/auth-client";
 
 export default function CreateAccountPage() {
+  async function loginWithInternetIdentity(e: any) {
+    e.preventDefault();
+
+    const authClient = await AuthClient.create();
+
+    await new Promise((resolve) => {
+      authClient.login({
+        onSuccess: resolve,
+      });
+    });
+
+    const principalId = authClient.getIdentity().getPrincipal().toText();
+    console.log(principalId);
+  }
+
+  async function loginWithEmail(e: any) {
+    e.preventDefault();
+
+    const authClient = await AuthClient.create();
+
+    const APP_NAME = "Link Sharing App";
+    const APP_LOGO = "https://nfid.one/icons/favicon-96x96.png";
+    const CONFIG_QUERY = `?applicationName=${APP_NAME}&applicationLogo=${APP_LOGO}`;
+    const identityProvider = `https://nfid.one/authenticate${CONFIG_QUERY}`;
+
+    await new Promise((resolve) => {
+      authClient.login({
+        identityProvider,
+        onSuccess: resolve,
+      });
+    });
+
+    const principalId = authClient.getIdentity().getPrincipal().toText();
+    console.log(principalId);
+  }
   return (
     <main className="md:w-[476px] flex flex-col gap-10 md:p-10 md:bg-White md:rounded-xl">
       <div className="flex flex-col gap-2">
@@ -15,34 +51,30 @@ export default function CreateAccountPage() {
         </p>
       </div>
 
-      <form className="flex flex-col gap-6">
-        <Input
-          icon="/images/icon-email.svg"
-          label="Email address"
-          name="email"
-          placeholder="e.g. alex@email.com"
-        ></Input>
-        <Input
-          icon="/images/icon-password.svg"
-          label="Create password"
-          name="createPassword"
-          placeholder="At least .8 characters"
-        ></Input>
-        <Input
-          icon="/images/icon-password.svg"
-          label="Confirm password"
-          name="confirmPassword"
-          placeholder="At least .8 characters"
-        ></Input>
-        <p className="text-sm text-DarkGrey">
-          Password must contain at least 8 characters
-        </p>
-        <Link
-          to=""
-          className={`flex justify-center items-center px-[27px] py-[11px] text-lg text-White bg-Purple rounded-lg hover:bg-LightPurple`}
-        >
-          Create new account
-        </Link>
+      <div className="flex flex-col gap-6">
+        <form onSubmit={loginWithEmail}>
+          <Button kind="1" className="flex gap-3 items-center justify-center">
+            <img
+              src="https://nfid.one/icons/favicon-96x96.png"
+              alt=""
+              width={20}
+              height={20}
+            />
+            Create with Email
+          </Button>
+        </form>
+
+        <form onSubmit={loginWithInternetIdentity}>
+          <Button kind="2" className="flex gap-3 items-center justify-center">
+            <img
+              src="https://cryptologos.cc/logos/internet-computer-icp-logo.svg?v=029"
+              alt=""
+              width={20}
+              height={20}
+            />
+            Create with Internet Identity
+          </Button>
+        </form>
 
         <p className="w-[70%] md:w-full text-base text-Grey self-center text-center">
           Already have an account?
@@ -50,7 +82,7 @@ export default function CreateAccountPage() {
             Login
           </Link>
         </p>
-      </form>
+      </div>
     </main>
   );
 }
--- src/link_sharing_app_frontend/src/components/LoginPage.tsx
+++ src/link_sharing_app_frontend/src/components/LoginPage.tsx
@@ -1,7 +1,45 @@
 import Input from "./UI/Inputs/Input";
 import { Link } from "react-router-dom";
+import { link_sharing_app_backend as backend } from "../../../declarations/link_sharing_app_backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { Actor } from "@dfinity/agent";
+import { NFID } from "@nfid/embed";
+import Button from "./UI/Button/Button";
 
 export default function LoginPage() {
+  async function loginWithInternetIdentity(e: any) {
+    e.preventDefault();
+    const authClient = await AuthClient.create();
+    await new Promise((resolve) => {
+      authClient.login({
+        onSuccess: resolve,
+      });
+    });
+
+    const principalId = authClient.getIdentity().getPrincipal().toText();
+    console.log(principalId);
+  }
+
+  async function loginWithEmail(e: any) {
+    e.preventDefault();
+
+    const authClient = await AuthClient.create();
+
+    const APP_NAME = "Link Sharing App";
+    const APP_LOGO = "https://nfid.one/icons/favicon-96x96.png";
+    const CONFIG_QUERY = `?applicationName=${APP_NAME}&applicationLogo=${APP_LOGO}`;
+    const identityProvider = `https://nfid.one/authenticate${CONFIG_QUERY}`;
+
+    await new Promise((resolve) => {
+      authClient.login({
+        identityProvider,
+        onSuccess: resolve,
+      });
+    });
+
+    const principalId = authClient.getIdentity().getPrincipal().toText();
+    console.log(principalId);
+  }
   return (
     <main className="md:w-[476px] flex flex-col gap-10 md:p-10 md:bg-White md:rounded-xl">
       <div className="flex flex-col gap-2">
@@ -13,26 +51,30 @@ export default function LoginPage() {
         </p>
       </div>
 
-      <form className="flex flex-col gap-6">
-        <Input
-          icon="/images/icon-email.svg"
-          label="Email address"
-          name="email"
-          placeholder="e.g. alex@email.com"
-        ></Input>
-        <Input
-          icon="/images/icon-password.svg"
-          label="Password"
-          name="password"
-          placeholder="Enter your password"
-        ></Input>
+      <div className="flex flex-col gap-6">
+        <form onSubmit={loginWithEmail}>
+          <Button kind="1" className="flex gap-3 items-center justify-center">
+            <img
+              src="https://nfid.one/icons/favicon-96x96.png"
+              alt=""
+              width={20}
+              height={20}
+            />
+            Login with Email
+          </Button>
+        </form>
 
-        <Link
-          to=""
-          className={`flex justify-center items-center px-[27px] py-[11px] text-lg text-White bg-Purple rounded-lg hover:bg-LightPurple`}
-        >
-          Login
-        </Link>
+        <form onSubmit={loginWithInternetIdentity}>
+          <Button kind="2" className="flex gap-3 items-center justify-center">
+            <img
+              src="https://cryptologos.cc/logos/internet-computer-icp-logo.svg?v=029"
+              alt=""
+              width={20}
+              height={20}
+            />
+            Login with Internet Identity
+          </Button>
+        </form>
 
         <p className="w-[70%] md:w-full text-base text-Grey self-center text-center">
           Don’t have an account?
@@ -40,7 +82,7 @@ export default function LoginPage() {
             Create account
           </a>
         </p>
-      </form>
+      </div>
     </main>
   );
 }
