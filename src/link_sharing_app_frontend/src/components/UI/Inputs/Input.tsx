6a506c194458d1aff477456358de7deec5a974c0
Abrham007 <abrhamaraya111@gmail.com>
2024-04-11 19:49:47 +0300

    added validation

--- src/link_sharing_app_frontend/src/components/UI/Inputs/Input.tsx
+++ src/link_sharing_app_frontend/src/components/UI/Inputs/Input.tsx
@@ -4,25 +4,48 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   name: string;
   icon?: string;
+  index: number;
+  register: any;
+  errors: any;
+  validation: any;
 }
 
-export default function Input({ label, name, icon, ...props }: InputProps) {
+export default function Input({
+  label,
+  name,
+  icon,
+  index,
+  register,
+  errors,
+  validation = {
+    required: { value: true, message: "Can’t be empty" },
+  },
+  ...props
+}: InputProps) {
+  let errorObj = errors?.links?.[index]?.[name];
   return (
     <div className="flex flex-col gap-1 cursor-pointer">
       <label htmlFor={name} className="text-sm text-DarkGrey">
         {label}
       </label>
-      <div className="flex gap-3 items-center px-4 py-3 bg-White border-[1px] border-solid border-Borders hover:border-Purple rounded-lg">
+      <div
+        className={`flex gap-3 items-center px-4 py-3 bg-White border-[1px] border-solid  ${
+          errorObj ? "border-Red" : "border-Borders"
+        } hover:border-Purple rounded-lg`}
+      >
         {icon && <img src={icon} alt="" width={16} height={16}></img>}
         <input
           {...props}
           id={name}
           type="text"
-          className="w-full text-base text-DarkGrey bg-transparent border-none outline-none"
+          className={`w-full text-base ${
+            errorObj ? "text-Red" : "text-DarkGrey"
+          }  bg-transparent border-none outline-none`}
+          {...register(`links.${index}.href`, validation)}
         />
-        {false && (
+        {errorObj && (
           <span className="ml-auto text-sm text-Red shrink-0">
-            Please check again
+            {errorObj?.message}
           </span>
         )}
       </div>
