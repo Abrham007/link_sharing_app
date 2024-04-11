b8de9b53c7873b65f9eaff1862fbc28295b04e8e
Abrham007 <abrhamaraya111@gmail.com>
2024-04-11 19:51:22 +0300

    made the component interactive and dynamic

--- src/link_sharing_app_frontend/src/components/DashboardPage/CustomLinkPage/LinkItem.tsx
+++ src/link_sharing_app_frontend/src/components/DashboardPage/CustomLinkPage/LinkItem.tsx
@@ -1,22 +1,67 @@
+import { Controller } from "react-hook-form";
 import Dropdown from "../../UI/Inputs/Dropdown";
 import Input from "../../UI/Inputs/Input";
 
-export default function LinkItem() {
+export default function LinkItem({
+  index,
+  register,
+  errors,
+  remove,
+  control,
+}: {
+  index: number;
+  register: any;
+  errors: any;
+  remove: (i: number) => void;
+  control: any;
+}) {
   return (
     <li className="flex flex-col gap-3 p-5 bg-LightGrey rounded-xl">
       <div className="flex justify-between">
         <h2 className="flex gap-2 text-lg text-Grey font-bold ">
           <img src="/images/icon-drag-and-drop.svg" alt="" />
-          <span>Link #1</span>
+          <span>Link #{index + 1}</span>
         </h2>
-        <button className="text-base text-Grey bg-transparent">Remove</button>
+        <button
+          onClick={() => {
+            remove(index);
+          }}
+          className="text-base text-Grey bg-transparent"
+        >
+          Remove
+        </button>
       </div>
-      <Dropdown label="Platform" icon="/images/icon-github.svg"></Dropdown>
+      <Controller
+        control={control}
+        name={`links.${index}.id`}
+        render={({ field }) => (
+          <Dropdown
+            label="Platform"
+            value={field.value}
+            onChange={field.onChange}
+          ></Dropdown>
+        )}
+      ></Controller>
+
       <Input
         label="Link"
-        name="link"
+        name="href"
         placeholder="e.g. https://www.github.com/johnappleseed"
         icon="/images/icon-link.svg"
+        index={index}
+        register={register}
+        errors={errors}
+        validation={{
+          required: { value: true, message: "Can’t be empty" },
+          validate: (value: any, formValues: any) => {
+            let urlPattern =
+              /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+            if (!urlPattern.test(value)) {
+              return "Please check the URL";
+            }
+            return true;
+          },
+        }}
       ></Input>
     </li>
   );
