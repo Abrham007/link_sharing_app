525f03f58e043334e9b3bfe679080653f13c15c0
Abrham007 <abrhamaraya111@gmail.com>
2024-04-12 14:26:46 +0300

    added validation

--- src/link_sharing_app_frontend/src/components/DashboardPage/ProfileDetailsPage/TextInput.tsx
+++ src/link_sharing_app_frontend/src/components/DashboardPage/ProfileDetailsPage/TextInput.tsx
@@ -5,6 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   register: any;
   error: any;
+  validation?: any;
 }
 
 export default function TextInput({
@@ -12,6 +13,7 @@ export default function TextInput({
   name,
   register,
   error,
+  validation,
   ...props
 }: InputProps) {
   return (
@@ -19,17 +21,23 @@ export default function TextInput({
       <label htmlFor={name} className="text-sm md:text-base text-Grey">
         {label}
       </label>
-      <div className="md:w-[344px] xl:w-[432px] flex gap-3 items-center px-4 py-3 bg-White border-[1px] border-solid border-Borders hover:border-Purple rounded-lg cursor-pointer">
+      <div
+        className={`md:w-[344px] xl:w-[432px] flex gap-3 items-center px-4 py-3 bg-White border-[1px] border-solid border-Borders ${
+          error ? "border-Red" : "border-Borders"
+        } hover:border-Purple rounded-lg cursor-pointer`}
+      >
         <input
           {...props}
           id={name}
           type="text"
-          className="w-full text-base text-DarkGrey bg-transparent border-none outline-none"
-          {...register(`${name}`, { required: true })}
+          className={`w-full text-base ${
+            error ? "text-Red" : "text-DarkGrey"
+          } bg-transparent border-none outline-none`}
+          {...register(`${name}`, validation)}
         />
         {error && (
           <span className="ml-auto text-sm text-Red shrink-0">
-            Please check again
+            {error.message}
           </span>
         )}
       </div>
