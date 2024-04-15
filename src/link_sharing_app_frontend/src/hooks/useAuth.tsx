6ad6a97a7dce41414115ae2967ff37fa1d1c1b32
Abrham007 <abrhamaraya111@gmail.com>
2024-04-15 19:39:56 +0300

    made the hook reusable

--- src/link_sharing_app_frontend/src/hooks/useAuth.tsx
+++ src/link_sharing_app_frontend/src/hooks/useAuth.tsx
@@ -1,27 +1,73 @@
-import React, { createContext, useContext, useMemo, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { AuthClient } from "@dfinity/auth-client";
+import { useNavigate } from "react-router-dom";
+import { link_sharing_app_backend as backend } from "../../../declarations/link_sharing_app_backend";
+import { UserData } from "../interface/UserData";
+import { Principal } from "@dfinity/principal";
 
-const AuthContext = createContext({
-  user: null,
-  login: (s: string) => {},
-  logout: () => {},
-});
+export type AuthContextType = {
+  id: string | undefined;
+  userData: UserData | null;
+  login: (connect: () => Promise<AuthClient>) => {};
+  createAccount: (connect: () => Promise<AuthClient>) => {};
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser]: [string | null, any] = useState(null);
+  const [id, setId] = useState<string>();
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const navigate = useNavigate();
 
-  function login(userPrincipal: string) {
-    setUser(userPrincipal);
+  async function login(connect: () => Promise<AuthClient>) {
+    const authClient = await connect();
+    let principalId = authClient.getIdentity().getPrincipal();
+    setId(principalId.toText());
+
+    let response = await backend.hasAccount(principalId);
+
+    if (response) {
+      let response = await backend.getUser(principalId);
+      setUserData(response);
+      console.log(userData);
+      navigate("/dashboard/links");
+    } else {
+      navigate("/");
+    }
   }
 
-  function logout() {}
+  async function createAccount(connect: () => Promise<AuthClient>) {
+    const authClient = await connect();
+    let principalId = authClient.getIdentity().getPrincipal();
+    setId(principalId.toText());
+
+    let response = await backend.createUser(principalId);
+    console.log(response);
+
+    if (response) {
+      let response = await backend.getUser(principalId);
+      setUserData(response);
+
+      navigate("/dashboard/links");
+    } else {
+      navigate("/");
+    }
+  }
 
   const value = useMemo(
     () => ({
-      user,
+      id,
+      userData,
       login,
-      logout,
+      createAccount,
     }),
-    [user]
+    [userData]
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
