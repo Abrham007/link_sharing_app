ddae504c21afa0b14a83391c659c9028aa7d210a
Abrham007 <abrhamaraya111@gmail.com>
2024-04-15 16:26:07 +0300

    corrected a logic bug

--- src/link_sharing_app_frontend/src/components/ProtectedLayout.tsx
+++ src/link_sharing_app_frontend/src/components/ProtectedLayout.tsx
@@ -1,15 +1,31 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, json } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { link_sharing_app_backend as backend } from "../../../declarations/link_sharing_app_backend";
+import { useEffect, useState } from "react";
+import { Principal } from "@dfinity/principal";
+import { AuthClient } from "@dfinity/auth-client";
 
 export default function ProtectedLayout() {
-  const { user } = useAuth();
+  const [userData, setUserData] = useState({});
+
+  // ndfdb-izceb-c36o6-e3xcd-w4y7q-3iw5w-3qndk-wbvs3-3e3kv-bsnp3-aae
 
   if (false) {
     return <Navigate to="/"></Navigate>;
   }
+
   return (
     <>
-      <Outlet></Outlet>
+      <Outlet context={{ userData }}></Outlet>
     </>
   );
+}
+
+export async function loader() {
+  const authClient = AuthClient.create();
+  const userPrincipal = (await authClient).getIdentity().getPrincipal();
+  const data = await backend.getUser(userPrincipal);
+  console.log(data);
+
+  return data;
 }
