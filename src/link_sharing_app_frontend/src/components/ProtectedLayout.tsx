9ca1e92b200ef60db0f0b39a246bfe0e5c8c65dc
Abrham007 <abrhamaraya111@gmail.com>
2024-04-15 19:39:33 +0300

    removed some unused code

--- src/link_sharing_app_frontend/src/components/ProtectedLayout.tsx
+++ src/link_sharing_app_frontend/src/components/ProtectedLayout.tsx
@@ -1,22 +1,21 @@
 import { Navigate, Outlet, json } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import { AuthContextType, useAuth } from "../hooks/useAuth";
 import { link_sharing_app_backend as backend } from "../../../declarations/link_sharing_app_backend";
 import { useEffect, useState } from "react";
 import { Principal } from "@dfinity/principal";
 import { AuthClient } from "@dfinity/auth-client";
 
 export default function ProtectedLayout() {
-  const [userData, setUserData] = useState({});
-
+  const { userData } = useAuth() as AuthContextType;
   // ndfdb-izceb-c36o6-e3xcd-w4y7q-3iw5w-3qndk-wbvs3-3e3kv-bsnp3-aae
 
-  if (false) {
+  if (!userData) {
     return <Navigate to="/"></Navigate>;
   }
 
   return (
     <>
-      <Outlet context={{ userData }}></Outlet>
+      <Outlet></Outlet>
     </>
   );
 }
@@ -25,7 +24,6 @@ export async function loader() {
   const authClient = AuthClient.create();
   const userPrincipal = (await authClient).getIdentity().getPrincipal();
   const data = await backend.getUser(userPrincipal);
-  console.log(data);
 
   return data;
 }
