48906152c87dda065bcacddcc622a1b9a22a63b1
Abrham007 <abrhamaraya111@gmail.com>
2024-04-17 15:35:16 +0300

    made the components more reusable

--- src/link_sharing_app_frontend/src/components/PreviewPage/PreviewItem.tsx
+++ src/link_sharing_app_frontend/src/components/PreviewPage/PreviewItem.tsx
@@ -1,5 +1,11 @@
 import { LinkList } from "../../util/linkList";
-export default function PreviewItem({ id }: { id: string }) {
+export default function PreviewItem({
+  id,
+  page = "preview",
+}: {
+  id: string;
+  page?: string;
+}) {
   const link = LinkList.find((link) => link.id === id);
 
   if (!link) {
@@ -8,16 +14,20 @@ export default function PreviewItem({ id }: { id: string }) {
 
   return (
     <li
-      className={`w-[237px] flex gap-2 items-center p-4 rounded-xl border border-solid border-Borders`}
+      className={`w-[237px] flex gap-2 items-center ${
+        page === "preview" ? "p-4 rounded-xl" : "px-4 py-[11px] rounded-lg"
+      }   border border-solid border-Borders`}
       style={{ backgroundColor: link.color }}
     >
       <div
-        className={`${link.id === "l2" ? "bg-[#737373]" : "bg-White"} w-5 h-5`}
+        className={`${
+          link.id === "link-2" ? "bg-[#737373]" : "bg-White"
+        } w-5 h-5`}
         style={{ mask: `url(${link.icon}) no-repeat center` }}
       ></div>
       <span
-        className="text-base text-White"
-        style={{ color: link.id === "l2" ? "#333" : "#FFF" }}
+        className={`${page === "preview" ? "text-base" : "text-sm"} text-White`}
+        style={{ color: link.id === "link-2" ? "#333" : "#FFF" }}
       >
         {link.title}
       </span>
@@ -28,7 +38,7 @@ export default function PreviewItem({ id }: { id: string }) {
         fill="none"
         viewBox="0 0 16 16"
         className="ml-auto"
-        style={{ fill: link.id === "l2" ? "#333" : "#FFF" }}
+        style={{ fill: link.id === "link-2" ? "#333" : "#FFF" }}
       >
         <path d="M2.667 7.333v1.334h8L7 12.333l.947.947L13.227 8l-5.28-5.28L7 3.667l3.667 3.666h-8Z" />
       </svg>
