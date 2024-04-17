db3391839a946a344e8f620b012e63a64020c9cc
Abrham007 <abrhamaraya111@gmail.com>
2024-04-17 15:34:58 +0300

    added the DashboardPreview component

--- src/link_sharing_app_frontend/src/components/DashboardPage/DashboardLayout.tsx
+++ src/link_sharing_app_frontend/src/components/DashboardPage/DashboardLayout.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
 import DashboardHeader from "./DashboardHeader";
+import DashboardPreview from "./DashboardPreview";
 
 export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-4 md:gap-6 min-h-screen p-4 md:p-6 bg-LightGrey">
       <DashboardHeader></DashboardHeader>
       <main className="w-full lg:max-w-[1392px] mx-auto flex gap-6 justify-center">
-        <section className="hidden flex-1 lg:flex justify-center items-center max-w-[560px] h-[834px] px-[25px] bg-White rounded-xl ">
-          <img src="/images/illustration-phone-mockup.svg" alt="" />
-        </section>
+        <DashboardPreview></DashboardPreview>
         <Outlet></Outlet>
       </main>
     </div>
