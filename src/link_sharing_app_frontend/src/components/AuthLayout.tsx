bbb75931423348f80c35da950c0b73d933a97fa4
Abrham007 <abrhamaraya111@gmail.com>
2024-04-18 09:44:25 +0300

    added user data provider

--- src/link_sharing_app_frontend/src/components/AuthLayout.tsx
+++ src/link_sharing_app_frontend/src/components/AuthLayout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "../hooks/useAuth";
+import { useEffect, useState } from "react";
+import { AuthClient } from "@dfinity/auth-client";
+import { UserDataContextProvider } from "../hooks/useUserData";
 
 export default function AuthLayout() {
   return (
     <AuthProvider>
-      <Outlet></Outlet>
+      <UserDataContextProvider>
+        <Outlet></Outlet>
+      </UserDataContextProvider>
     </AuthProvider>
   );
 }
