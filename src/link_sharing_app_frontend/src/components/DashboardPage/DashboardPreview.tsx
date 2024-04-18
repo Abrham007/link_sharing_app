fb42cf7361386f591102dcb5aa98fb2fd38c81e7
Abrham007 <abrhamaraya111@gmail.com>
2024-04-18 09:42:13 +0300

    made component dynamic

--- src/link_sharing_app_frontend/src/components/DashboardPage/DashboardPreview.tsx
+++ src/link_sharing_app_frontend/src/components/DashboardPage/DashboardPreview.tsx
@@ -1,36 +1,61 @@
+import { useUserData } from "../../hooks/useUserData";
+import { UserData } from "../../interface/UserData";
 import PreviewItem from "../PreviewPage/PreviewItem";
 
 export default function DashboardPreview() {
-  let imgSrc = "/images/avatar-edit-3.jpg";
+  const { userData }: { userData: UserData } = useUserData();
+
+  let hasImage = userData?.profile.profilePic.length > 0;
+  let hasFirstName = userData?.profile.firstName !== "";
+  let hasLastName = userData?.profile.lastName !== "";
+  let hasEmail = userData?.profile.email !== "";
+  let hasLinks = userData?.links.length > 0;
+
+  let imageContent = new Uint8Array(userData.profile.profilePic);
+  let imgUrl = URL.createObjectURL(
+    new Blob([imageContent.buffer], { type: "image/jpeg" })
+  );
+
   return (
     <section className="hidden flex-1 lg:flex justify-center items-center max-w-[560px] h-[834px] px-[25px] bg-White rounded-xl ">
       <div className="relative w-[307px] h-[631px] mx-auto">
         <img src="/images/illustration-phone-mockup.svg" alt="" />
-        <figure className="absolute top-[53.5px] left-0 right-0 w-max mx-auto flex flex-col gap-6 items-center text-center bg-White">
-          <img
-            src={imgSrc}
-            alt=""
-            width={104}
-            height={104}
-            className={`w-[104px] h-[104px] object-contain rounded-full border-[4px] bordrer-solid border-Purple `}
-          />
-          <figcaption className="flex flex-col gap-[43px]">
-            <div className="flex flex-col gap-2">
-              <h2 className="text-[1.125rem] leading-[1.6875rem] font-semibold text-DarkGrey">
-                Ben Wright
-              </h2>
-              <p className="text-[0.875rem] leading-[1.3125rem] text-Grey">
-                ben@example.com
+        <div className="w-[273px] flex flex-col gap-[43px] absolute top-[63px] left-0 right-0 mx-auto items-center text-center">
+          <div className="w-full flex flex-col items-center gap-5">
+            <div className="w-[96px] h-[96px]">
+              {hasImage && (
+                <img
+                  src={imgUrl}
+                  alt=""
+                  width={96}
+                  height={96}
+                  className={`w-[96px] h-[96px] rounded-full border-[4px] bordrer-solid border-Purple`}
+                />
+              )}
+            </div>
+
+            <div className="w-full flex flex-col gap-1">
+              <div className="h-[27px]">
+                <h2 className="text-[1.125rem] leading-[1.6875rem] font-semibold text-DarkGrey bg-White">
+                  {hasFirstName && userData.profile.firstName}{" "}
+                  {hasLastName && userData.profile.lastName}
+                </h2>
+              </div>
+
+              <p className="text-[0.875rem] leading-[1.3125rem] text-Grey bg-White">
+                {hasEmail && userData.profile.email}
               </p>
             </div>
-            <ul className="max-h-[300px] flex flex-col gap-5 px-2 pb-3 overflow-auto">
-              <PreviewItem id={"link-1"} page="dashboard"></PreviewItem>
-              <PreviewItem id={"link-2"} page="dashboard"></PreviewItem>
-              <PreviewItem id={"link-3"} page="dashboard"></PreviewItem>
-              <PreviewItem id={"link-4"} page="dashboard"></PreviewItem>
+          </div>
+
+          {hasLinks && (
+            <ul className="w-full max-h-[300px] flex flex-col gap-5 items-center px-2 pb-3 overflow-auto bg-White">
+              {userData.links.map((link) => (
+                <PreviewItem id={link.id} page="dashboard"></PreviewItem>
+              ))}
             </ul>
-          </figcaption>
-        </figure>
+          )}
+        </div>
       </div>
     </section>
   );
