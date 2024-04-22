8b8169c719deca542aa9a4538086fa338d73e8bf
Abrham007 <abrhamaraya111@gmail.com>
2024-04-22 11:02:14 +0300

    made the component more reusable

--- src/link_sharing_app_frontend/src/components/Message.tsx
+++ src/link_sharing_app_frontend/src/components/Message.tsx
@@ -3,22 +3,26 @@ import { useEffect } from "react";
 export default function Message({
   icon,
   text,
+  error,
   handleCloseMessage,
 }: {
   icon: string;
   text: string;
+  error: boolean;
   handleCloseMessage: () => void;
 }) {
   useEffect(() => {
     setTimeout(() => {
       handleCloseMessage();
-    }, 2000);
+    }, 3000);
   }, []);
 
   return (
     <div className="fixed bottom-10 left-[50%] -translate-x-1/2 flex gap-2  px-6 py-4 bg-DarkGrey rounded-xl shadow-[0px_0px_32px_0px_rgba(0,0,0,0.10)]">
       <img src={icon} alt="" />
-      <p className="w-max text-lg text-LightGrey">{text}</p>
+      <p className={`w-max text-lg ${error ? "text-Red" : "text-LightGrey"}`}>
+        {text}
+      </p>
     </div>
   );
 }
